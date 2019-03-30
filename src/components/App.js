dd85ab483045e39f201d5c28cc7cb94c91151f62
Mary Healy <maryhealy1978@gmail.com>
2019-03-30 16:07:36 +0100

    completed

---
+++ src/components/App.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Player from './Player';
+import AddPlayerForm from './AddPlayerForm';
+
+class App extends Component {
+  state = {
+    players: [
+      {
+        name: "Guil",
+        score: 0,
+        id: 1
+      },
+      {
+        name: "Treasure",
+        score: 0,
+        id: 2
+      },
+      {
+        name: "Ashley",
+        score: 0,
+        id: 3
+      },
+      {
+        name: "James",
+        score: 0,
+        id: 4
+      }
+    ]
+  };
+
+
+  handleScoreChange= (index, score) => {
+    this.setState( prevState => ({
+      score: prevState.players[index].score += score
+    }));
+  }
+
+  prevPlayerId = 4;
+
+  handleAddPlayer = (name) => {
+    this.setState( prevState => {
+      return {
+        players: [
+          ...prevState.players,
+          {
+            name,
+            score: 0,
+            id: this.prevPlayerId += 1
+          }
+        ]
+      };
+    });
+  }
+
+  handleRemovePlayer = (id) => {
+    this.setState( prevState => {
+      return {
+        players: prevState.players.filter(p => p.id !== id)
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header
+          title="Scoreboard"
+          players={this.state.players}
+        />
+
+        {/* Players list */}
+        {this.state.players.map( (player, index) =>
+          <Player
+            name={player.name}
+            score={player.score}
+            id={player.id}
+            index={index}
+            key={player.id.toString()}
+            changeScore={this.handleScoreChange}
+            removePlayer={this.handleRemovePlayer}
+          />
+        )}
+
+        <AddPlayerForm addPlayer={this.handleAddPlayer}/>
+      </div>
+    );
+  }
+}
+
+export default App;
